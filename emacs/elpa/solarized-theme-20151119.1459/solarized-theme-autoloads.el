22f2068c2b2e3e5f89a74b5f647ca0ff0f286df7
Felipe Balbi <balbi@ti.com>
2015-11-29 10:15:59 -0600

    emacs: package updates
    Signed-off-by: Felipe Balbi <balbi@ti.com>

---
+++ emacs/elpa/solarized-theme-20151119.1459/solarized-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; solarized-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "solarized" "solarized.el" (22107 9375 429731
+;;;;;;  571000))
+;;; Generated autoloads from solarized.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
+;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
+;;;;;;  (22107 9375 494574 833000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; solarized-theme-autoloads.el ends here
