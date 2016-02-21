707b956a66f6771e872aa61d142123eae65e6eb8
Felipe Balbi <balbi@kernel.org>
2016-02-21 20:17:04 +0200

    emacs: reduce font size
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- emacs/init.el
+++ emacs/init.el
@@ -191,4 +191,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "default" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
