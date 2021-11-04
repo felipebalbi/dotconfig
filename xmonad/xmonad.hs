31c8955ebc26ae5eb99cacdd3448105b1e0a1adb
Felipe Balbi <balbi@kernel.org>
2021-11-04 08:36:12 +0200

    xmonad: clean-up a bit
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -38,24 +38,14 @@ import XMonad.Hooks.SetWMName
 
 -- Layouts
 import XMonad.Layout.Grid
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.Spiral
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.TwoPane
 
 -- Layouts modifiers
 import XMonad.Layout.LayoutModifier
-import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
-import XMonad.Layout.Magnifier
-import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
-import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
 import XMonad.Layout.Spacing
-import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
-import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
-import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
 
 -- Prompt
 import XMonad.Prompt
@@ -224,11 +214,26 @@ myKeys =
   , ("M-<Backspace>", promote)
   , ("M-C-<Tab>", rotSlavesDown)
 
+    -- Push window back into tiling
+  , ("M-t", withFocused $ windows . W.sink)
+
+    -- Increment the number of windows in the master area
+  , ("M-,", sendMessage (IncMasterN 1))
+
+    -- Decrement the number of windows in the master area
+  , ("M-.", sendMessage (IncMasterN (-1)))
+
+    -- Shrink the master area
+  , ("M-h", sendMessage Shrink)
+
+    -- Expand the master area
+  , ("M-l", sendMessage Expand)
+
     -- Emacs
   , ("M-e", spawn "emacsclient -c -a ''")
 
     -- Scrot
-  , ("M-C-s", spawn "sleep 0.2 && scrot -s /tmp/%Y-%m-%d_%H-%M-%S_$wx$h.png")
+  , ("M-C-s", spawn "sleep 0.2 && scrot -s /tmp/%Y-%m-%d_%H-%M-%S_%w-%h.png")
 
     -- Multimedia Keys
   , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute")
@@ -289,16 +294,23 @@ searchList = [ ("a", archwiki)
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
-myLayout = avoidStruts $ mySpacing 8 $ (tiled ||| Mirror tiled ||| Full ||| focus ||| three ||| grid)
+myLayout = avoidStruts . (mySpacing 6) $
+      tiled
+  ||| Mirror tiled
+  ||| twopane
+  ||| Mirror twopane
+  ||| Full
+  ||| three
+  ||| grid
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
 
+     -- two panes
+     twopane = TwoPane delta ratio
+
      --- ThreeColumns layout
      three   = ThreeCol nmaster delta ratio
-
-     --- ThreeColumnsMid
-     focus   = ThreeColMid nmaster delta ratio
 
      --- Grid layout
      grid    = GridRatio (4/3)
@@ -382,54 +394,3 @@ main = do
                            }
     , startupHook        = myStartupHook
     } `additionalKeysP` myKeys
-
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help = unlines ["The default modifier key is 'Super'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Enter        Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
