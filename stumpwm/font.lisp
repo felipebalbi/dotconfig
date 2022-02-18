6451e62c6c4d47da900ab71c507b032f9cb165d2
Felipe Balbi <felipe@balbi.sh>
2022-02-18 09:56:53 +0200

    stumpwm: customize preferred font
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ stumpwm/font.lisp
@@ -0,0 +1,9 @@
+(require :ttf-fonts)
+
+(setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
+(setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME")
+                                                 "/.fonts/font-cache.sexp"))
+(xft:cache-fonts)
+
+(set-font (make-instance 'xft:font :family "Fira Code Retina"
+                                   :subfamily "Regular" :size 11))
