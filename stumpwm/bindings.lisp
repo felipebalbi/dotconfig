f42260cf2c6183aa5c84910a1997766f289e8208
Felipe Balbi <felipe@balbi.sh>
2022-02-18 08:10:53 +0200

    stumpwm: bindings: add more bindings
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- stumpwm/bindings.lisp
+++ stumpwm/bindings.lisp
@@ -1,7 +1,38 @@
-(defcommand my/terminal (&optional prg) ()
-  (run-shell-command
-   (if prg
-       (format nil "st -e ~a" prg)
-       "st")))
+(defun define-keys (map alist)
+  (loop for (key . cmd) in alist
+        do (define-key map (kbd key) cmd)))
 
-(define-key *top-map* (kbd "s-RET") "my/terminal")
+(defparameter *my/bindings*
+  '(("s-RET" . "exec st")
+
+    ("s-b"   . "move-focus left")
+    ("s-f"   . "move-focus right")
+    ("s-p"   . "move-focus up")
+    ("s-n"   . "move-focus down")
+
+    ("s-B"   . "move-window left")
+    ("s-F"   . "move-window right")
+    ("s-P"   . "move-window up")
+    ("s-N"   . "move-window down")
+
+    ("s-h"   . "hsplit")
+    ("s-v"   . "vsplit")
+    ("s-C-F" . "fullscreen")
+
+    ("s-r"   . "resize")
+
+    ("s-1"   . "gselect 1")
+    ("s-2"   . "gselect 2")
+    ("s-3"   . "gselect 3")
+    ("s-4"   . "gselect 4")
+    ("s-5"   . "gselect 5")
+    ("s-6"   . "gselect 6")
+    ("s-7"   . "gselect 7")
+    ("s-8"   . "gselect 8")
+    ("s-9"   . "gselect 9")
+    ("s-0"   . "gselect 10")
+
+    ("s-e"   . "exec emacs")
+    ))
+
+(define-keys *top-map* *my/bindings*)
